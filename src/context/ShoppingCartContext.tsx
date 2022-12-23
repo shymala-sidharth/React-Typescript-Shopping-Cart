2c90f4020b5dddfcd0100eb37d1641b0b81884bc
Shymala Sidharth <shymala.sidharth22@gmail.com>
2022-12-23 13:44:41 +1300

    working on logic

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,15 +1,30 @@
-import { createContext, useContext, ReactNode } from "react";
+import { createContext, useContext, ReactNode, useState } from "react";
 type ShoppingCartProviderProps = {
   children: ReactNode
 }
-const ShoppingCartContext = createContext({})
+
+type CartItem = {
+  id: number
+  quantity: number
+}
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number
+  increaseCartQuantity: (id: number) => void
+  decreaseCartQuantity: (id: number) => void
+  removeFromCart: (id: number) => void
+}
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
 
 export function useShoppingCare() {
   return useContext(ShoppingCartContext)
 }
 
 
-export function ShoppingCartProvider({children}) {
+export function ShoppingCartProvider({children} : ShoppingCartProviderProps) {
+
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
   return <ShoppingCartContext.Provider value={{}}>
     {children}
   </ShoppingCartContext.Provider>
