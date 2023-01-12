b9e1b0f61976eafa9a5556235ee78abab7eedfc3
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-01-12 13:55:51 +1300

    working on Auth0

---
+++ src/components/AuthNav.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import AuthenticationButton from "../auth/Authentication-Button";
+
+export function AuthNav() {
+  return (
+    <div>
+    <AuthenticationButton/>
+    </div>
+  );
+}
+
